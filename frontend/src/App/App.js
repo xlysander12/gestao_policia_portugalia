aa6acb00bf38e5848d3c12c59b47f992d8afbce9
Tiago <xlysander12pt@gmail.com>
2024-01-19 17:30:09 +0000

    Não faço puta ideia

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -3,6 +3,7 @@ import {Route, Routes, BrowserRouter} from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login/login";
 import Dashboard from "./pages/Dashboard/dashboard";
+import OfficerInfo from "./pages/OfficerInfo/officerinfo";
 
 class App extends Component {
     render() {
@@ -12,6 +13,7 @@ class App extends Component {
                     <Routes>
                         <Route exact path="/" element={<Dashboard/>}/>
                         <Route path="/login" element={<Login/>}/>
+                        <Route path="/officerinfo" element={<OfficerInfo/>}/>
                     </Routes>
                 </BrowserRouter>
             </div>
