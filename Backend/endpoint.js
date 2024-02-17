917db7a895c43b5bf404d95c478b2f284e8d15c9
Tiago <xlysander12pt@gmail.com>
2024-02-17 17:22:56 +0000

    [Backend] Tokens are now stored in the database instead of an object in the backend

--- Backend/endpoint.js
+++ Backend/endpoint.js
@@ -20,87 +20,14 @@ const poolDefaultGNR = mysql.createPool(dbConfigDefaultGNR);
 // Storing Tokens List
 const authenticatedTokens = {"dev": {nif: 668855471, forces: ["psp", "gnr"], lastUsed: "NE"}};
 
+// Forces list
+const forces = ["psp", "gnr"];
+
 // React Static
 app.use(express.static(path.resolve(__dirname + "/../frontend/build")));
 
 // CSS & JS static
 app.use("/cssjs", express.static(path.resolve(__dirname + "/../Frontend-old/"), { extensions: ["js", "css"] }));
-
-/************************************
- * Token / Authentication Functions *
- ***********************************/
-
-function generateToken(username) {
-    // Before generating a token, seek and revoke all tokens from the user
-    for (let token in authenticatedTokens) {
-        if (authenticatedTokens[token] === username) {
-            delete authenticatedTokens[token];
-        }
-    }
-
-    // Generate a random token
-    let token = "";
-    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for (let i = 0; i < 32; i++) {
-        token += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-
-    // After generating the token, check if it already exists
-    if (authenticatedTokens[token]) {
-        // If it already exists, generate another one
-        return generateToken();
-    }
-
-    // If it doesn't exist, store it in authenticatedTokens and return it
-    authenticatedTokens[token] = username;
-    return token;
-}
-
-async function checkTokenValidityIntents(token, force, intent) {
-    // Check if the token is present
-    if (token === undefined) {
-        return [false, 401, "Não foi fornecido um token de autenticação."];
-    }
-
-    // Check if the token exists
-    if (!authenticatedTokens[token]) {
-        return [false, 401, "O token fornecido é inválido."];
-    }
-
-    // Making sure force is specified
-    if (force === undefined) {
-        return [false, 400, "Não foi fornecida uma força para a validação do token."];
-    }
-
-    // Check if the user has the force
-    if (authenticatedTokens[token].forces.indexOf(force) === -1) {
-        return [false, 403, "Não tens permissão para acessar a dados desta força"];
-    }
-
-    // If intent is null, then the user doesn't need special permissions
-    if (intent === null || intent === undefined) {
-        return [true, 0]; // Since the return was true, no HTTT Status Code is needed
-    }
-
-    // Fetch the database the intents json of the user
-    const [rows, fields] = await queryDB(force, `SELECT intents FROM usuarios WHERE username = ${authenticatedTokens[token]}`);
-
-    if (rows.length === 0) {
-        return [false, 500, "Algo de errado ocorreu. Tente novamente mais tarde"];
-    }
-
-    // Converting the row to a JSON object
-    let userIntents = JSON.parse(rows[0].intents);
-
-    // Check if the user has the intent
-    if (userIntents[intent]) {
-        return [true, 200];
-    }
-
-    return [false, 403, "Não tens permissão para realizar esta ação"];
-
-}
 
 /************************************
  * Database Functions *
@@ -116,6 +43,94 @@ async function queryDB(force, query) {
     }
 }
 
+/************************************
+ * Token / Authentication Functions *
+ ***********************************/
+
+async function generateToken(nif) {
+    // Before generating a token, making sure the user doesn't have any tokens already, and if it does, delete them
+    for (const force in forces) {
+        await queryDB(force, `DELETE FROM tokens WHERE nif = ${nif}`)
+    }
+
+    // Repeat the generation process until an unique token is generated
+    let unique = false;
+    let token = "";
+    while (!unique) {
+        // Generate a random token
+        const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for (let i = 0; i < 32; i++) {
+            token += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+
+        // After generating the token, check if it already exists
+        let [rows, fields] = await queryDB("psp", `SELECT * FROM tokens WHERE token = ${token}`);
+        if (rows.length !== 0) {
+            continue;
+        }
+
+        [rows, fields] = await queryDB("gnr", `SELECT * FROM tokens WHERE token = ${token}`);
+        if (rows.length !== 0) {
+            continue;
+        }
+
+        // If it doesn't exist, exit out of the loop
+        unique = true;
+    }
+
+    // Return the token
+    return token;
+}
+
+async function checkTokenValidityIntents(token, force, intent) {
+    // Check if the token is present
+    if (token === undefined) {
+        return [false, 401, "Não foi fornecido um token de autenticação."];
+    }
+
+    // Making sure force is specified
+    if (force === undefined) {
+        return [false, 400, "Não foi fornecida uma força para a validação do token."];
+    }
+
+    // Querying the Database to check if the token exists
+    let [rows, fields] = await queryDB(force, `SELECT nif FROM tokens WHERE token = ${token}`);
+    if (rows.length === 0) {
+        return [false, 401, "O token fornecido não é válido."];
+    }
+
+    // Store the nif the token points to
+    let nif = rows[0].nif;
+
+    // Once it has been confirmed the token exists, the lastUsed field should be updated in all databases
+    // It's used the `then()` method to avoid waiting for the query to finish since the result is not needed
+    for (const forcesKey in forces) {
+        queryDB(forcesKey, `UPDATE tokens SET ultimouso = CURRENT_TIMESTAMP() WHERE token = ${token}`).then(_ => {});
+    }
+
+    // If intent is null, then the user doesn't need special permissions
+    if (intent === null || intent === undefined) {
+        return [true, 0]; // Since the return was true, no HTTT Status Code is needed
+    }
+
+    // Fetch from the database the intents json of the user
+    [rows, fields] = await queryDB(force, `SELECT intents FROM usuarios WHERE nif = ${nif}`);
+
+    // Converting the row to a JSON object
+    let userIntents = JSON.parse(rows[0].intents);
+
+    // Check if the user has the intent
+    try {
+        if (!userIntents[intent]) {
+            return [false, 403, "Não tens permissão para realizar esta ação"];
+        }
+    } catch {
+        return [false, 400, "Permissões inválidas"];
+    }
+
+
+    return [true, 200];
+}
 
 /********************************
  * Routes to serve the frontend [DEPRECATED]*
@@ -202,55 +217,56 @@ app.post("/api/validateToken", async (req, res) => {
 // TODO: The login endpoint needs to check which force the user belongs to or even all of them
 app.post("/api/login", async (req, res) => {
     // Check if the request has the correct body
-    if (!req.body.username || !req.body.password) {
+    if (!req.body.nif || !req.body.password) {
         res.status(400).json({
             message: "Não foi fornecido um username ou password",
         });
         return;
     }
 
-    // Check if the user exists
+    // Check if the user exists (it's needed to check on all forces databases)
+    let found_results = [];
 
-    const [rows, fields] = await poolDefaultPSP.query(`SELECT password FROM usuarios WHERE username = ${req.body.username}`);
+    // Getting the row corresponding the nif and adding it to the found_results array
+    for (const force in forces) {
+        const [rows, fields] = await queryDB(force, `SELECT password FROM usuarios WHERE nif = ${req.body.nif}`);
+        found_results.push(...rows);
+    }
 
-    // If the user doesn't exist, return an error
-    if (rows.length === 0) {
+    // If the found_results array is empty, then the username doesn't exist
+    if (found_results.length === 0) {
         res.status(401).json({
             message: "O username fornecido não existe."
         });
         return;
     }
 
+    let valid_login;
+
     // If the password is NULL, then the correct password would be "seguranca"
-    if (rows[0].password === null) {
-        if (req.body.password !== "seguranca") {
-            res.status(401).json({
-                message: "Password incorreta."
-            });
-            return;
-        }
+    if (found_results[0].password === null) {
+        valid_login = req.body.password === "seguranca";
+    } else {
+        // If the password is not NULL, check if it is correct
+        valid_login = rows[0].password === req.body.password; // TODO: Hash password
+    }
 
-        // If everything is correct, generate a token and store it in the authenticatedTokens list
-        let token = generateToken(req.body.username);
 
-        // Send the token to the user
-        res.status(200).json({
-            message: "Operação bem sucedida",
-            data: token
+    // If the password is incorrect, return a 401 status code
+    if (!valid_login) {
+        res.status(401).json({
+            message: "Password incorreta"
         });
         return;
     }
 
-    // If the password is not NULL, check if it is correct
-    if (rows[0].password !== req.body.password) { // TODO: Hash the password
-        res.status(401).json({
-            message: "Password incorreta."
-        })
-        return;
-    }
+    // If everything is correct, generate a token
+    const token = generateToken(req.body.nif);
 
-    // If everything is correct, generate a token and store it in the authenticatedTokens list
-    let token = generateToken(req.body.username);
+    // After generating the token, store it in the databases of the forces the user belongs to
+    for (const force in forces) {
+        await queryDB(force, `INSERT INTO tokens (token, nif) SELECT "${token}", ${req.body.nif} FROM dual WHERE EXISTS ( SELECT 1 FROM usuarios WHERE nif = ${req.body.nif})`);
+    }
 
     // Send the token to the user
     res.status(200).json({
