eab09fc8cd1b423083aba9407ceeb111507229aa
Tiago <xlysander12pt@gmail.com>
2024-03-05 16:14:03 +0000

    [Frontend] Something with previews, idk

--- frontend/src/dev/previews.jsx
+++ frontend/src/dev/previews.jsx
@@ -1,12 +1,16 @@
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
 import {PaletteTree} from './palette'
 import OfficerList from "../App/components/OfficerList/officerlist";
+import OfficerInfo from "../App/pages/OfficerInfo/officerinfo";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
             <ComponentPreview path="/OfficerList">
                 <OfficerList/>
+            </ComponentPreview>
+            <ComponentPreview path="/OfficerInfo">
+                <OfficerInfo/>
             </ComponentPreview>
         </Previews>
     )
