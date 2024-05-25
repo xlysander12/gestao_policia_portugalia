feb953603292e01f99614543f038e82d0181f613
Tiago <xlysander12pt@gmail.com>
2024-05-25 22:41:04 +0100

    [Frontend] Created a module to easily handle requests to the backend

---
+++ frontend/src/App/utils/requests.js
@@ -0,0 +1,34 @@
+// This function will handle all requests to the backend
+// It will be used by all components that need to make requests
+// It will be a wrapper around the fetch API, making it easier to use
+async function make_request(url, method, body= undefined, force= undefined, useAuth= true) {
+    // First, make sure the URL starts with a slash
+    if (!url.startsWith('/')) {
+        url = '/' + url;
+    }
+
+    // Next, make sure the method is all caps
+    method = method.toUpperCase();
+
+    // Next, make the actual request
+    let response = await fetch(url, {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': useAuth ? localStorage.getItem('token') : undefined,
+            'X-Portalseguranca-Force': force ? force : localStorage.getItem('force')
+        },
+        body: body !== undefined ? JSON.stringify(body) : undefined
+    });
+
+    // After that, get the code from the response, if it is higher than 500, assume something went wrong and try to reaload the current page
+    if (response.status >= 500) {
+        window.location.reload();
+        return;
+    }
+
+    // Finally, return the response
+    return response;
+}
+
+exports.make_request = make_request;
