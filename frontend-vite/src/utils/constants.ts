1ece18ffcc072e7229b227598f76c765f54df821
Tiago Carvalho <xlysander12pt@gmail.com>
2024-07-18 19:26:46 +0100

    [Frontend] Migrated utils functions

---
+++ frontend-vite/src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const base_api_url = "/portugalia/portalseguranca/api";
+export const base_url = "/portugalia/portalseguranca";
---
+++ frontend-vite/src/utils/requests.ts
@@ -0,0 +1,38 @@
+// This function will handle all requests to the backend
+// It will be used by all components that need to make requests
+// It will be a wrapper around the fetch API, making it easier to use
+import {base_api_url} from "./constants";
+
+export async function make_request(url: string, method: ("GET" | "POST" | "PATCH" | "DELETE"), body = undefined, force: string | undefined = undefined, useAuth= true, useBaseAPIURL= true) {
+    // First, make sure the URL starts with a slash
+    if (!url.startsWith('/')) {
+        url = '/' + url;
+    }
+
+    // If the useBaseAPIURL is true, then add the base URL to the request
+    if (useBaseAPIURL) {
+        url = base_api_url + url;
+    }
+
+    // Next, make the actual request
+
+    let response = await fetch(url, {
+        method: method,
+        // @ts-ignore
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': useAuth ? localStorage.getItem('token') : null,
+            'X-Portalseguranca-Force': force ? force : localStorage.getItem('force')
+        },
+        body: body !== undefined ? JSON.stringify(body) : undefined
+    });
+
+    // After that, get the code from the response, if it is higher than 500, assume something went wrong and try to reaload the current page
+    // TODO: Add a toast with the error code
+    if (response.status >= 500) {
+        window.location.reload();
+    }
+
+    // Finally, return the response
+    return response;
+}
