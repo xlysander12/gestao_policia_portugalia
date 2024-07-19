478cf508e52d4b31ef1c9cc572ab67c5b69dcda9
Tiago Carvalho <xlysander12pt@gmail.com>
2024-07-19 04:33:33 +0100

    [Frontend] Added support for backend proxy in development mode

--- frontend-vite/vite.config.ts
+++ frontend-vite/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {base_api_url, base_url} from "./src/utils/constants";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    base: base_url,
+    server: {
+        proxy: {
+            [base_api_url]: {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
+    }
 })
