f153d4dacddce933559c6c8290ea22d61aa59c3c
Tiago Carvalho <xlysander12pt@gmail.com>
2024-07-19 04:35:00 +0100

    [Frontend] Migrated "Dashboard" page

--- frontend-vite/src/App.tsx
+++ frontend-vite/src/App.tsx
@@ -1,33 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar/navbar.tsx";
+import {base_url} from "./utils/constants.ts";
+import Dashboard from "./pages/Dashboard/dashboard.tsx";
+import {Bounce, ToastContainer} from "react-toastify";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter basename={base_url}>
+          <Navbar />
+          <div style={{height: "cacl(100vh - calc(4rem + 16px))"}}>
+              <Routes>
+                  {/*Login route, doesn't need the PrivateRoute Component*/}
+                  <Route path={"/login"} element={null} />
+
+                  {/*Routes that require the user to be logged in*/}
+                  <Route path={"/"} element={<Dashboard />} />
+              </Routes>
+          </div>
+      </BrowserRouter>
+
+        <ToastContainer
+            position={"top-right"}
+            autoClose={5000}
+            hideProgressBar={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            pauseOnHover
+            theme={"dark"}
+            transition={Bounce}
+        />
     </>
   )
 }
---
+++ frontend-vite/src/pages/Dashboard/dashboard.tsx
@@ -0,0 +1,12 @@
+import {ReactElement} from "react";
+
+function Dashboard(): ReactElement {
+    return (
+        <div>
+            {/*<Navbar />*/}
+            <h1>Dashboard</h1>
+        </div>
+    );
+}
+
+export default Dashboard;
