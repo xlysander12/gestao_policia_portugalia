5f62df51a220a4612de43603b177b4abf647dd54
Tiago Carvalho <xlysander12pt@gmail.com>
2024-08-16 04:55:33 +0100

    [Backend] Added a route to serve the database structure file and all the existing backups

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,5 +1,6 @@
 // Importing basic libraries
 import express, {Router} from "express";
+import serveIndex from "serve-index";
 import {config} from "dotenv";
 import {join} from "path";
 config({path: join(__dirname, "..", ".env")});
@@ -13,14 +14,17 @@ const app = Router(); // This app is a router to compartimentalize routes
 // TODO: Make sure every environment variable is set
 
 // React Static
-app.use(express.static(join(__dirname, "..", "Frontend", "dist")));
+app.use(express.static(join(__dirname, "..", "..", "Frontend", "dist")));
 
 // Import the API routes
 app.use("/api", apiRoutes);
 
+// Database backup files
+app.use("/db", express.static(join(__dirname, "..", "..", "Database")), serveIndex(join(__dirname, "..", "..", "Database"), {icons: true}));
+
 // React Build
 app.get("/*", (req, res) => {
-    res.sendFile(join(__dirname, "..", "frontend", "dist", "index.html"));
+    res.sendFile(join(__dirname, "..", "..", "Frontend", "dist", "index.html"));
 });
 
 
