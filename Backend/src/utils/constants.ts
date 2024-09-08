459fc193bffa02cea781e1784a428e9e7ef71c69
Tiago Carvalho <xlysander12pt@gmail.com>
2024-09-08 16:58:17 +0100

    [Backend] Fixed typo in endpoint name that would cause inevitable errors

--- Backend/src/utils/constants.ts
+++ Backend/src/utils/constants.ts
@@ -7,8 +7,8 @@ export const PASSWORD_SALT_ROUNDS = 10;
 
 
 // Necessary information for the request
-export const NO_TOKEN_REQUIRED_ROUTES = [[/\/util\/.*/, "*"], [/\/account\/login/, "POST"]];
-export const NO_FORCE_REQUIRED_ROUTES = [[/\/account\/login/, "POST"]]
+export const NO_TOKEN_REQUIRED_ROUTES = [[/\/util\/.*/, "*"], [/\/accounts\/login/, "POST"]];
+export const NO_FORCE_REQUIRED_ROUTES = [[/\/accounts\/login/, "POST"]]
 
 export type IntentRequiredRoute = {
     route: RegExp,
