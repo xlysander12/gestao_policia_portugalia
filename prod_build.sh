d57fdf72e5541fd5bec15a355b7e3fbb7d5de084
Tiago Carvalho <xlysander12pt@gmail.com>
2024-09-20 16:43:09 +0100

    [FullStack] Created a script to automate the process of building the app in production

---
+++ prod_build.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/bash
+# This script will delete the 'dist' folder in all projects and then build them
+# Start working in the API Types
+cd API\ Types || exit
+
+# Delete 'dist' folder in API Types
+rm -rf dist
+
+# Create a clean install of all modules
+npm ci
+
+# Build API Types
+npm run build
+
+# Move out of API Types
+cd ..
+
+echo "API Types built successfully"
+pause
+
+# Start working in the Backend
+cd Backend || exit
+
+# Delete 'dist' folder in Backend
+rm -rf dist
+
+# Create a clean install of all modules
+npm ci
+
+# Build Backend
+npm run build
+
+# Move out of Backend
+cd ..
+
+echo "Backend built successfully"
+pause
+
+# Start working in the Frontend
+cd Frontend || exit
+
+# Delete 'dist' folder in Frontend
+rm -rf dist
+
+# Create a clean install of all modules
+npm ci
+
+# Build Frontend
+npm run build
+
+# Move out of Frontend
+cd ..
+
+echo "Frontend built successfully"
+pause
+
+# Create the node_modules folder in the Backend dist folder
+mkdir Backend/dist/node_modules
+
+# Create the @portalseguranca folder in the Backend dist node_modules folder
+mkdir Backend/dist/node_modules/@portalseguranca
+
+# Create the api-types folder in the Backend dist node_modules @portalseguranca folder
+mkdir Backend/dist/node_modules/@portalseguranca/api-types
+
+# Move the dist contents from API Types to Backend
+cp -r API\ Types/dist/* Backend/dist/node_modules/@portalseguranca/api-types
+
+echo "Build Completed"
