45aa2d3ecf1c18b6d0ad4e7a4bad349299738dec
Tiago Carvalho <xlysander12pt@gmail.com>
2024-12-25 05:03:04 +0000

    [Backend] Fixed small typo in comment

--- Backend/src/api/officers/activity/hours/repository/index.ts
+++ Backend/src/api/officers/activity/hours/repository/index.ts
@@ -7,7 +7,7 @@ export type OfficerHoursHistoryType = Omit<OfficerSpecificHoursType, "week_start
     week_end: Date
 }
 export async function fetchHoursHistory(force: string, officer: number, filters: Filters): Promise<OfficerHoursHistoryType[]> {
-    // Get the hours of the Officer from the repository
+    // Get the hours of the Officer from the database
     const result = await queryDB(force, `SELECT * FROM officer_hours ${filters.query}`, filters.values);
 
     // Build a proper object to hold all information
