e818bacc6f3aac9f904b538b5f1146febeffeb0f
Tiago Carvalho <xlysander12pt@gmail.com>
2025-01-10 00:57:51 +0000

    [Frontend] Added the active justification field in the OfficerInfo Activity Tab
    Also moved all API queries of the Activity Tab to their own Pairs to improve performance and readability

--- Frontend/src/pages/OfficerInfo/activity-panel.tsx
+++ Frontend/src/pages/OfficerInfo/activity-panel.tsx
@@ -6,24 +6,68 @@ import Gate from "../../components/Gate/gate.tsx";
 import {Divider, Skeleton} from "@mui/material";
 import {make_request} from "../../utils/requests.ts";
 import {
-    OfficerLastShiftResponse,
-    OfficerSpecificHoursResponse
+    OfficerActiveJustification, OfficerActiveJustificationsResponse,
+    OfficerLastShiftResponse, OfficerSpecificHoursResponse
 } from "@portalseguranca/api-types/officers/activity/output";
 import {UpdateOfficerLastShiftBodyType} from "@portalseguranca/api-types/officers/activity/input";
 import {toHoursAndMinutes} from "../../utils/misc.ts";
-import {WeekHoursRegistryModal} from "../Activity/modals";
+import {InactivityJustificationModal, WeekHoursRegistryModal} from "../Activity/modals";
+import {ForceDataContext, getObjectFromId} from "../../force-data-context.ts";
 
 type LastShiftPairProps = {
-    date: Date | null,
-    onDateChange?: (date: Date) => void
+    officer: number
 }
-const LastShiftPair = ({date, onDateChange}: LastShiftPairProps) => {
+const LastShiftPair = ({officer}: LastShiftPairProps) => {
     // Get the logged user context
     const loggedUser = useContext(LoggedUserContext);
 
-    const [dateState, setDate] = useState<Date | null>(date);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [lastShift, setLastShift] = useState<Date | null>(null);
     const [editMode, setEditMode] = useState<boolean>(false);
 
+    async function fetchLastShift() {
+        // Set the loading to true and editmode to false
+        setLoading(true);
+        setEditMode(false);
+
+        // Clear current data
+        setLastShift(null);
+
+        // Fetch the API the last shift date
+        let response = await make_request(`/officers/${officer}/activity/last-shift`, "GET");
+
+        if (response.status === 404) {
+            setLoading(false);
+            return;
+        }
+
+        const responseJson: OfficerLastShiftResponse = await response.json();
+
+        setLastShift(new Date(responseJson.data.last_shift));
+
+        // Set loading to false
+        setLoading(false);
+    }
+
+    async function updateOfficerLastShift() {
+        // Set the loading to true
+        setLoading(true);
+
+        // Build the request body
+        const requestBody: UpdateOfficerLastShiftBodyType = {
+            last_shift: lastShift!.toISOString()
+        }
+
+        // Make the request
+        await make_request(`/officers/${officer}/activity/last-shift`, "PUT", {body: requestBody});
+
+        // Update the last shift date
+        await fetchLastShift();
+    }
+
+    useEffect(() => {
+        fetchLastShift();
+    }, [officer]);
 
     return (
         <div className={style.informationPairDiv}>
@@ -35,76 +79,194 @@ const LastShiftPair = ({date, onDateChange}: LastShiftPairProps) => {
                     gap: "10px"
                 }}
             >
-                <Gate show={!editMode}>
-                    <DefaultTypography
-                        sx={{marginTop: "4px"}}
-                        clickable={loggedUser.intents["activity"]}
-                        onClick={() => {
-                            if (loggedUser.intents["activity"]) {
-                                setEditMode(true);
-                            }
-                        }}
-                    >
-                        {dateState !== null ? `${dateState.getDate()}/${dateState.getMonth()}/${dateState.getFullYear()}`: "N/A"}
-                    </DefaultTypography>
-                </Gate>
-
-                <Gate show={editMode}>
-                    <DefaultTextField
-                        required
-                        sameTextColorWhenDisabled
-                        disabled={!editMode}
-                        type={"date"}
-                        value={dateState !== null ? dateState.toISOString().split("T")[0]: ""}
-                        onChange={(event) => setDate(new Date(Date.parse(event.target.value)))}
+                <Gate show={loading}>
+                    <Skeleton
+                        variant={"text"}
+                        animation={"wave"}
+                        width={"30%"}
                     />
                 </Gate>
 
-                <Gate show={loggedUser.intents["activity"] && editMode}>
-                    <DefaultButton
-                        size={"small"}
-                        buttonColor={"lightgreen"}
-                        onClick={() => {
-                            // Call the onDateChange function
-                            if (onDateChange) {
-                                onDateChange(dateState!);
-                            }
-
-                            // Disable the editing mode for the last shift date
-                            setEditMode(false);
-                        }}
+                <Gate show={!loading}>
+                    <Gate show={!editMode}>
+                        <DefaultTypography
+                            sx={{marginTop: "4px"}}
+                            clickable={loggedUser.intents["activity"]}
+                            onClick={() => {
+                                if (loggedUser.intents["activity"]) {
+                                    setEditMode(true);
+                                }
+                            }}
                         >
-                        Guardar
-                    </DefaultButton>
+                            {lastShift !== null ? `${lastShift.getDate()}/${lastShift.getMonth()}/${lastShift.getFullYear()}`: "N/A"}
+                        </DefaultTypography>
+                    </Gate>
+
+                    <Gate show={editMode}>
+                        <DefaultTextField
+                            required
+                            sameTextColorWhenDisabled
+                            disabled={!editMode}
+                            type={"date"}
+                            value={lastShift !== null ? lastShift.toISOString().split("T")[0]: ""}
+                            onChange={(event) => setLastShift(new Date(event.target.value))}
+                        />
+                    </Gate>
+
+                    <Gate show={loggedUser.intents["activity"] && editMode}>
+                        <DefaultButton
+                            size={"small"}
+                            buttonColor={"lightgreen"}
+                            onClick={() => {
+                                // Call the onDateChange function
+                                updateOfficerLastShift();
+                            }}
+                        >
+                            Guardar
+                        </DefaultButton>
+                    </Gate>
                 </Gate>
             </div>
         </div>
     );
 }
 
-type LastWeekHoursProps = {
-    id: number | null
-    minutes: number | null
+type LastWeekHoursPairProps = {
     officer: number
 }
-const LastWeekHours = ({id, minutes, officer}: LastWeekHoursProps) => {
+const LastWeekHoursPair = ({officer}: LastWeekHoursPairProps) => {
+    const [loading, setLoading] = useState<boolean>(true);
     const [isModalOpen, setModalOpen] = useState<boolean>(false);
+    const [lastHours, setLastHours] = useState<{id: number, minutes: number} | null>(null);
+
+    async function fetchLastWeekHours() {
+        // Ensure loading is true
+        setLoading(true);
+
+        // Clear current data
+        setLastHours(null);
+
+        // Fetch the API the last week hours
+        let response = await make_request(`/officers/${officer}/activity/hours/last`, "GET");
+
+        // If response is 404, there are no registry of this officer
+        if (response.status === 404) {
+            setLoading(false);
+            return;
+        }
+
+        const responseJson: OfficerSpecificHoursResponse = await response.json();
+        setLastHours({
+            id: responseJson.data.id,
+            minutes: responseJson.data.minutes
+        });
+
+        // Set loading to false
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchLastWeekHours();
+    }, [officer]);
 
     return (
         <>
             <div className={style.informationPairDiv}>
                 <label>Últimos minutos (horas):</label>
-                <DefaultTypography
-                    sx={{marginTop: "4px"}}
-                    color={minutes ? (minutes < 300 ? "red": "var(--portalseguranca-color-text-light)"): "var(--portalseguranca-color-text-light)"}
-                    clickable={!!minutes}
-                    onClick={() => setModalOpen(true)}
-                >
-                    {minutes ? minutes : "N/A"} ({minutes ? toHoursAndMinutes(minutes) : "N/A"})
-                </DefaultTypography>
+                <Gate show={loading}>
+                    <Skeleton
+                        variant={"text"}
+                        animation={"wave"}
+                        width={"30%"}
+                    />
+                </Gate>
+                <Gate show={!loading}>
+                    <DefaultTypography
+                        sx={{marginTop: "4px"}}
+                        color={lastHours?.minutes ? (lastHours?.minutes < 300 ? "red": "var(--portalseguranca-color-text-light)"): "var(--portalseguranca-color-text-light)"}
+                        clickable={!!lastHours?.minutes}
+                        onClick={() => setModalOpen(true)}
+                    >
+                        {lastHours?.minutes ? lastHours?.minutes : "N/A"} ({lastHours?.minutes ? toHoursAndMinutes(lastHours?.minutes) : "N/A"})
+                    </DefaultTypography>
+                </Gate>
             </div>
 
-            <WeekHoursRegistryModal open={isModalOpen} onClose={() => setModalOpen(false)} officer={officer} entryId={id ? id: undefined}/>
+            <WeekHoursRegistryModal open={isModalOpen} onClose={() => setModalOpen(false)} officer={officer} entryId={lastHours?.id ? lastHours?.id: undefined}/>
+        </>
+    )
+}
+
+type ActiveJustificationPairProps = {
+    officer: number
+}
+const ActiveJustificationPair = ({officer}: ActiveJustificationPairProps) => {
+    // Get force data from context
+    const forceData = useContext(ForceDataContext);
+
+    const [loading, setLoading] = useState<boolean>(true);
+    const [isModalOpen, setModalOpen] = useState<boolean>(false);
+    const [activeJustification, setActiveJustification] = useState<OfficerActiveJustification | null>(null);
+
+    async function fetchActiveJustification() {
+        // Ensure loading is true
+        setLoading(true);
+
+        // Clear current data
+        setActiveJustification(null);
+
+        // Fetch the API the last week hours
+        let response = await make_request(`/officers/${officer}/activity/justifications/active`, "GET");
+
+        const responseJson: OfficerActiveJustificationsResponse = await response.json();
+
+        // If there are no justifications, set loading to false and return
+        if (responseJson.data.length === 0) {
+            setLoading(false);
+            return;
+        }
+
+        // If there's multiple justifications, prioritize the one with the highest type
+        if (responseJson.data.length > 1) {
+            responseJson.data.sort((a: OfficerActiveJustification, b: OfficerActiveJustification) => {
+                return b.type - a.type;
+            });
+        }
+
+        setActiveJustification(responseJson.data[0]);
+
+        // Set loading to false
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchActiveJustification();
+    }, [officer]);
+
+    return (
+        <>
+            <div className={style.informationPairDiv}>
+                <label>Justificação ativa:</label>
+                <Gate show={loading}>
+                    <Skeleton
+                        variant={"text"}
+                        animation={"wave"}
+                        width={"30%"}
+                    />
+                </Gate>
+                <Gate show={!loading}>
+                    <DefaultTypography
+                        sx={{marginTop: "4px"}}
+                        color={activeJustification ? (getObjectFromId(activeJustification.type, forceData.inactivity_types)!.color): "var(--portalseguranca-color-text-light)"}
+                        clickable={!!activeJustification}
+                        onClick={() => setModalOpen(true)}
+                    >
+                        {activeJustification ? getObjectFromId(activeJustification?.type, forceData.inactivity_types)!.name : "N/A"}
+                    </DefaultTypography>
+                </Gate>
+            </div>
+
+            <InactivityJustificationModal open={isModalOpen} onClose={() => setModalOpen(false)} officerNif={officer} justificationId={activeJustification?.id!}/>
         </>
     )
 }
@@ -113,108 +275,16 @@ type ActivityPanelProps = {
     nif: number
 }
 export const ActivityPanel = ({nif}: ActivityPanelProps) => {
-    const [loading, setLoading] = useState<boolean>(true);
-    const [lastShift, setLastShift] = useState<Date | null>(null);
-    const [lastWeekHours, setLastWeekHours] = useState<{ id: number, minutes: number } | null>(null);
-
-    const fetchLastShift = async () => {
-        // Fetch the API the last shift date
-        let response = await make_request(`/officers/${nif}/activity/last-shift`, "GET");
-
-        if (response.status === 404) {
-            return null;
-        }
-
-        const responseJson = await response.json();
-
-        setLastShift(new Date(Date.parse((responseJson as OfficerLastShiftResponse).data.last_shift)));
-    }
-
-    const fetchLastWeekHours = async () => {
-        // Fetch the API the last week hours
-        let response = await make_request(`/officers/${nif}/activity/hours/last`, "GET");
-
-        if (response.status === 404) {
-            return null;
-        }
-
-        const responseJson: OfficerSpecificHoursResponse = await response.json();
-
-        setLastWeekHours({
-            id: responseJson.data.id,
-            minutes: responseJson.data.minutes
-        });
-    }
-
-    const updateOfficerLastShift = async (date: Date) => {
-        // Set the loading to true
-        setLoading(true);
-
-        // Build the request body
-        const requestBody: UpdateOfficerLastShiftBodyType = {
-            last_shift: date.toISOString()
-        }
-
-        // Make the request
-        await make_request(`/officers/${nif}/activity/last-shift`, "PUT", {body: requestBody});
-
-        // Update the last shift date
-        await fetchLastShift();
-
-        // Set the loading to false
-        setLoading(false);
-    }
-
-    const updateOfficerActivity = async () => {
-        // Update the last shift date
-        await fetchLastShift();
-
-        // Update the last week's hours
-        await fetchLastWeekHours();
-
-        // Set the loading to false
-        setLoading(false);
-    }
-
-    useEffect(() => {
-        updateOfficerActivity();
-    }, [nif]);
-
-    let content;
-    if (loading) {
-        content = (
-            <>
-                {Array.from({length: 6}, (_, index) => {
-                    return (
-                        <>
-                            <Skeleton
-                                key={`Skeleton${index}`}
-                                variant={"text"}
-                                animation={"wave"}
-                                width={index % 2 === 0 ? "100%": "30%"}
-                            />
-                            {index % 2 === 0 ? <Divider flexItem/>: null}
-                        </>
-                    );
-                })}
-            </>
-        );
-    } else {
-        content = (
-            <>
-                <div className={style.officerInfoInnerFieldsetDiv}>
-                    <LastShiftPair date={lastShift} onDateChange={updateOfficerLastShift}/>
-                    <Divider/>
-                    <LastWeekHours id={lastWeekHours?.id!} minutes={lastWeekHours?.minutes!} officer={nif}/>
-                </div>
-            </>
-        );
-    }
-
     return (
         <fieldset>
             <legend>Atividade</legend>
-            {content}
+            <div className={style.officerInfoInnerFieldsetDiv}>
+                <ActiveJustificationPair officer={nif} />
+                <Divider/>
+                <LastShiftPair officer={nif}/>
+                <Divider/>
+                <LastWeekHoursPair officer={nif}/>
+            </div>
         </fieldset>
     );
 }
