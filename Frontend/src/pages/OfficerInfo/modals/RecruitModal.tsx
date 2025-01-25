49abfdce87f8098fa3012880bda015e3409d7a0e
Tiago Carvalho <xlysander12pt@gmail.com>
2025-01-25 18:27:07 +0000

    [Frontend] Added support to restore former Officers

--- Frontend/src/pages/OfficerInfo/modals/RecruitModal.tsx
+++ Frontend/src/pages/OfficerInfo/modals/RecruitModal.tsx
@@ -3,10 +3,11 @@ import {useImmer} from "use-immer";
 import {useNavigate} from "react-router-dom";
 import {make_request} from "../../../utils/requests.ts";
 import {toast} from "react-toastify";
-import {Modal, ModalSection} from "../../../components/Modal/modal.tsx";
+import {ConfirmationDialog, Modal, ModalSection} from "../../../components/Modal/modal.tsx";
 import modalsStyle from "./officerinfomodals.module.css";
 import {DefaultButton, DefaultTextField} from "../../../components/DefaultComponents";
 import {Checkbox, FormControlLabel} from "@mui/material";
+import {OfficerInfoGetResponse} from "@portalseguranca/api-types/officers/output";
 
 type RecruitModalProps = {
     open: boolean
@@ -23,6 +24,14 @@ function RecruitModal({open, onClose}: RecruitModalProps): ReactElement {
         discord: "",
         steam: "",
         recruit: false
+    });
+
+    // State that hold the confirmation dialog
+    const [restoreDialog, setRestoreDialog] = useImmer({
+        open: false,
+        last_asked_nif: 0,
+        last_asked_name: "",
+        is_error: false
     });
 
     // Navigate hook
@@ -56,129 +65,200 @@ function RecruitModal({open, onClose}: RecruitModalProps): ReactElement {
         navigate(`/efetivos/${officerInfo.nif}`);
     }
 
+    const checkOfficer = async (nif: number) => {
+        // Set the restore dialog error flag to false
+        setRestoreDialog((draft) => {
+            draft.is_error = false
+        });
+
+        // Make sure the NIF is valid
+        if (isNaN(nif)) return;
+
+        // Compare the NIF with the last asked one
+        if (restoreDialog.last_asked_nif === nif) return;
+
+        // Make the request to check if there is a former officer with the same NIF
+        const officerCheckRequest = await make_request(`/officers/${nif}`, "GET");
+
+        // * Check if the response is 404
+        // If it is, the officer is not a former officer
+        if (officerCheckRequest.status === 404) {
+            return;
+        }
+
+        // Get the former officer's data
+        const officerCheckResponse: OfficerInfoGetResponse = await officerCheckRequest.json();
+
+        // Check if the entered officer isn't already an active one
+        if (!officerCheckResponse.meta.former) {
+            setRestoreDialog((draft) => {
+                draft.is_error = true
+            });
+            toast.error("Esse efetivo já é ativo na força!");
+            return;
+        }
+
+        // * If it's not an active one, then we can open the restore dialog since it's a former one
+        // Set the restore dialog state with the former officer's data
+        setRestoreDialog((draft) => {
+            draft.open = true;
+            draft.last_asked_nif = officerCheckResponse.data.nif;
+            draft.last_asked_name = officerCheckResponse.data.name;
+        });
+    }
+
+    const restoreOfficer = async (nif: number) => {
+        // Make the request to restore the officer
+        const restoreRequest = await make_request(`/officers/${nif}/restore`, "POST");
+
+        // Output the response to a toast
+        toast((await restoreRequest.json()).message, {type: restoreRequest.ok ? "success" : "error"});
+
+        // If the request was successful, navigate to the page of the restored officer
+        if (restoreRequest.ok) {
+            navigate(`/efetivos/${nif}`);
+        }
+    }
+
     return (
-        <Modal width={"37%"} open={open} onClose={onClose} title={"Recrutar novo efetivo"}>
-            {/*@ts-ignore*/}
-            <form onSubmit={recruitMember}>
-                <ModalSection title={"Informações Pessoais"}>
-                    <div className={modalsStyle.formDiv}>
-                        {/* TODO: add proper titles to explain the custom patterns */}
-                        <DefaultTextField
-                            fullWidth
-                            label={"Nome"}
-                            type={"text"}
-                            onChange={(event) => setOfficerInfo(draft => {draft.name = event.target.value})}
-                            error={officerInfo.name !== "" && !(/^([a-zA-Z ]|[à-ü ]|[À-Ü ])+$/.test(officerInfo.name))}
-                            sx={{margin: "10px 0 0 0"}}
-                            required
-                            inputProps={{
-                                name: "officerName",
-                                pattern: "^([a-zA-Z ]|[à-ü ]|[À-Ü ])+$"
-                            }}
-                        />
+        <>
+            <Modal width={"37%"} open={open} onClose={onClose} title={"Recrutar novo efetivo"}>
+                {/*@ts-ignore*/}
+                <form onSubmit={recruitMember}>
+                    <ModalSection title={"Informações Pessoais"}>
+                        <div className={modalsStyle.formDiv}>
+                            {/* TODO: add proper titles to explain the custom patterns */}
+                            <DefaultTextField
+                                fullWidth
+                                label={"Nome"}
+                                type={"text"}
+                                onChange={(event) => setOfficerInfo(draft => {draft.name = event.target.value})}
+                                error={officerInfo.name !== "" && !(/^([a-zA-Z ]|[à-ü ]|[À-Ü ])+$/.test(officerInfo.name))}
+                                sx={{margin: "10px 0 0 0"}}
+                                required
+                                inputProps={{
+                                    name: "officerName",
+                                    pattern: "^([a-zA-Z ]|[à-ü ]|[À-Ü ])+$"
+                                }}
+                            />
 
-                        <DefaultTextField
-                            fullWidth
-                            label={"NIF"}
-                            type={"text"}
-                            onChange={(event) => setOfficerInfo(draft => {draft.nif = event.target.value})}
-                            error={officerInfo.nif !== "" && !(/^[0-9]{7,9}$/.test(officerInfo.nif))}
-                            sx={{margin: "10px 0 0 0"}}
-                            required
-                            inputProps={{
-                                name: "officerNIF",
-                                pattern: "^[0-9]{7,9}$"
-                            }}
-                        />
+                            <DefaultTextField
+                                fullWidth
+                                label={"NIF"}
+                                type={"text"}
+                                onChange={(event) => setOfficerInfo(draft => {draft.nif = event.target.value})}
+                                onBlur={() => {checkOfficer(parseInt(officerInfo.nif))}}
+                                error={officerInfo.nif !== "" && (!(/^[0-9]{7,9}$/.test(officerInfo.nif)) || restoreDialog.is_error)}
+                                sx={{margin: "10px 0 0 0"}}
+                                required
+                                inputProps={{
+                                    name: "officerNIF",
+                                    pattern: "^[0-9]{7,9}$"
+                                }}
+                            />
 
-                        <DefaultTextField
-                            fullWidth
-                            label={"Telemóvel"}
-                            type={"text"}
-                            onChange={(event) => setOfficerInfo(draft => {draft.phone = event.target.value})}
-                            error={officerInfo.phone !== "" && !(/^[0-9]{9}$/.test(officerInfo.phone))}
-                            sx={{margin: "10px 0 0 0"}}
-                            required
-                            inputProps={{
-                                name: "officerPhone",
-                                pattern: "^[0-9]{9}$"
-                            }}
-                        />
+                            <DefaultTextField
+                                fullWidth
+                                label={"Telemóvel"}
+                                type={"text"}
+                                onChange={(event) => setOfficerInfo(draft => {draft.phone = event.target.value})}
+                                error={officerInfo.phone !== "" && !(/^[0-9]{9}$/.test(officerInfo.phone))}
+                                sx={{margin: "10px 0 0 0"}}
+                                required
+                                inputProps={{
+                                    name: "officerPhone",
+                                    pattern: "^[0-9]{9}$"
+                                }}
+                            />
 
-                        <DefaultTextField
-                            fullWidth
-                            label={"IBAN"}
-                            type={"text"}
-                            onChange={(event) => setOfficerInfo(draft => {draft.iban = event.target.value})}
-                            error={officerInfo.iban !== "" && !(/^PT[0-9]{5,8}$/.test(officerInfo.iban))}
-                            sx={{margin: "10px 0 0 0"}}
-                            required
-                            inputProps={{
-                                name: "officerIBAN",
-                                pattern: "^PT[0-9]{5,8}$"
-                            }}
-                        />
+                            <DefaultTextField
+                                fullWidth
+                                label={"IBAN"}
+                                type={"text"}
+                                onChange={(event) => setOfficerInfo(draft => {draft.iban = event.target.value})}
+                                error={officerInfo.iban !== "" && !(/^PT[0-9]{5,8}$/.test(officerInfo.iban))}
+                                sx={{margin: "10px 0 0 0"}}
+                                required
+                                inputProps={{
+                                    name: "officerIBAN",
+                                    pattern: "^PT[0-9]{5,8}$"
+                                }}
+                            />
 
-                        <DefaultTextField
-                            fullWidth
-                            label={"KMs"}
-                            defaultValue={0}
-                            type={"number"}
-                            inputProps={{step: 100, name: "officerKMs"}}
-                            onChange={(event) => setOfficerInfo(draft => {draft.kms = Number(event.target.value)})}
-                            sx={{margin: "10px 0 0 0"}}
-                            required
-                        />
+                            <DefaultTextField
+                                fullWidth
+                                label={"KMs"}
+                                defaultValue={0}
+                                type={"number"}
+                                inputProps={{step: 100, name: "officerKMs"}}
+                                onChange={(event) => setOfficerInfo(draft => {draft.kms = Number(event.target.value)})}
+                                sx={{margin: "10px 0 0 0"}}
+                                required
+                            />
 
-                        <DefaultTextField
-                            fullWidth
-                            label={"Discord ID"}
-                            type={"text"}
-                            onChange={(event) => setOfficerInfo(draft => {draft.discord = event.target.value})}
-                            sx={{margin: "10px 0 0 0"}}
-                            required
-                            inputProps={{
-                                name: "officerDiscord"
-                            }}
-                        />
+                            <DefaultTextField
+                                fullWidth
+                                label={"Discord ID"}
+                                type={"text"}
+                                onChange={(event) => setOfficerInfo(draft => {draft.discord = event.target.value})}
+                                sx={{margin: "10px 0 0 0"}}
+                                required
+                                inputProps={{
+                                    name: "officerDiscord"
+                                }}
+                            />
 
-                        <DefaultTextField
-                            fullWidth
-                            label={"Steam ID / URL"}
-                            type={"text"}
-                            onChange={(event) => setOfficerInfo(draft => {draft.steam = event.target.value})}
-                            sx={{margin: "10px 0 0 0"}}
-                            error={officerInfo.steam !== "" && !(/^steam:[0-9]{9}$/.test(officerInfo.steam)) && !(/^http(s)?:\/\/steamcommunity.com\/id\/.+/.test(officerInfo.steam))}
-                            required
-                            inputProps={{
-                                name: "officerSteam",
-                                pattern: "(^steam:([0-9]|[a-z])+$)|(^http(s)?:\/\/steamcommunity\.com\/id\/.+$)"
-                            }}
-                        />
+                            <DefaultTextField
+                                fullWidth
+                                label={"Steam ID / URL"}
+                                type={"text"}
+                                onChange={(event) => setOfficerInfo(draft => {draft.steam = event.target.value})}
+                                sx={{margin: "10px 0 0 0"}}
+                                error={officerInfo.steam !== "" && !(/^steam:[0-9]{9}$/.test(officerInfo.steam)) && !(/^http(s)?:\/\/steamcommunity.com\/id\/.+/.test(officerInfo.steam))}
+                                required
+                                inputProps={{
+                                    name: "officerSteam",
+                                    pattern: "(^steam:([0-9]|[a-z])+$)|(^http(s)?:\/\/steamcommunity\.com\/id\/.+$)"
+                                }}
+                            />
 
-                        <FormControlLabel
-                            control={<Checkbox
-                                onChange={(event) => setOfficerInfo(draft => {draft.recruit = event.target.checked})}
-                            />}
-                            label={"Recrutar como Cadete"}
-                            sx={{
-                                margin: "10px 0 0 0",
+                            <FormControlLabel
+                                control={<Checkbox
+                                    onChange={(event) => setOfficerInfo(draft => {draft.recruit = event.target.checked})}
+                                />}
+                                label={"Recrutar como Cadete"}
+                                sx={{
+                                    margin: "10px 0 0 0",
 
-                            }}
-                        />
+                                }}
+                            />
 
-                    </div>
-                </ModalSection>
+                        </div>
+                    </ModalSection>
 
-                <DefaultButton
-                    type={"submit"}
-                    buttonColor={"green"}
-                    fullWidth
-                >
-                    Contratar
-                </DefaultButton>
-            </form>
-        </Modal>
+                    <DefaultButton
+                        type={"submit"}
+                        buttonColor={"green"}
+                        fullWidth
+                    >
+                        Contratar
+                    </DefaultButton>
+                </form>
+            </Modal>
+
+            <ConfirmationDialog
+                open={restoreDialog.open}
+                title={`Restaurar ${restoreDialog.last_asked_name}`}
+                text={
+                    `Existe um antigo efetivo com o mesmo NIF inserido (#${restoreDialog.last_asked_nif}):\n
+                    - ${restoreDialog.last_asked_name}\n
+                    Deseja restaurar este efetivo?`
+                }
+                onConfirm={() => restoreOfficer(restoreDialog.last_asked_nif)}
+                onDeny={() => setRestoreDialog((draft) => {
+                    draft.open = false
+            })} />
+        </>
     );
 }
 
