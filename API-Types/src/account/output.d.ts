f46f1dabb396940f4ae3f2832f4d2e00550e5ed9
Tiago Carvalho <xlysander12pt@gmail.com>
2025-04-15 03:34:19 +0100

    [Backend] Implemented Websocket Events for when an accounts gets changed

--- API-Types/src/account/output.d.ts
+++ API-Types/src/account/output.d.ts
@@ -1,4 +1,4 @@
-import {BaseResponse} from "../index";
+import {BaseResponse, SocketResponse} from "../index";
 
 export interface ValidateTokenResponse extends BaseResponse {
     data: number;
@@ -31,4 +31,20 @@ export interface UserForcesResponse extends BaseResponse {
     data: {
         forces: UserForce[]
     }
+}
+
+export interface AccountSocket extends SocketResponse {
+    nif: number
+}
+
+export interface AccountUpdateSocket extends AccountSocket {
+    action: "update"
+}
+
+export interface AccountManageSocket extends AccountSocket {
+    action: "manage"
+}
+
+export interface AccountDeleteSocket extends AccountSocket {
+    action: "delete"
 }
