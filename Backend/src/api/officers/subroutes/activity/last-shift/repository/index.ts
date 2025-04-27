c45edc79278cc73dafcc9b6c1e4f1500b36bfa82
Tiago Carvalho <xlysander12pt@gmail.com>
2025-04-27 22:46:22 +0100

    [Backend] Fixed error in Unix timestamp handling in the route to set an officer's last shift date

--- Backend/src/api/officers/subroutes/activity/last-shift/repository/index.ts
+++ Backend/src/api/officers/subroutes/activity/last-shift/repository/index.ts
@@ -10,6 +10,6 @@ export async function fetchLastShift(force: string, nif: number): Promise<Date |
     return result[0].last_shift as Date;
 }
 
-export async function updateLastShift(force: string, nif: number, last_shift: Date | null) {
-    return await queryDB(force, `INSERT INTO officer_last_shift (officer, last_shift) VALUES (?, ?) ON DUPLICATE KEY UPDATE last_shift = ?`, [nif, last_shift, last_shift]);
+export async function updateLastShift(force: string, nif: number, last_shift: number | null) {
+    return await queryDB(force, `INSERT INTO officer_last_shift (officer, last_shift) VALUES (?, FROM_UNIXTIME(?)) ON DUPLICATE KEY UPDATE last_shift = FROM_UNIXTIME(?)`, [nif, last_shift, last_shift]);
 }
