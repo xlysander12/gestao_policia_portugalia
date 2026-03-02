491161ed2d170de56d050ddeae42f8dc116c5d73
Tiago Carvalho <xlysander12pt@gmail.com>
2026-03-02 19:19:24 +0000

    [Backend] Created DB migration to create the audit-logs table
    Issue #160

---
+++ Backend/migrations/20260302185126_audit-log-table-creation.ts
@@ -0,0 +1,26 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable("audit_logs", table => {
+        table.increments("id").primary().notNullable();
+        table.integer("nif").notNullable()
+            .references("nif").inTable("officers")
+            .onUpdate("cascade").onDelete("cascade");
+
+        table.string("ip_address").nullable();
+        table.datetime("timestamp").notNullable().defaultTo(knex.fn.now());
+        table.string("module").notNullable();
+        table.enum("action", ["add", "update", "manage", "delete", "restore"]).notNullable();
+        table.string("type").nullable();
+        table.integer("target").nullable();
+        table.jsonb("details").nullable();
+        table.integer("status_code").notNullable();
+    });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists("audit_logs");
+}
+
