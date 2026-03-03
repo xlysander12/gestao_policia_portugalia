4721352bca50d7eea9d2f83edb8a4f8ea5ccb203
Tiago Carvalho <xlysander12pt@gmail.com>
2026-03-03 21:46:31 +0000

    [Backend] Ensure stored responseBody is parsed

--- Backend/src/middlewares/store-response.ts
+++ Backend/src/middlewares/store-response.ts
@@ -2,12 +2,12 @@ import express from "express";
 import {APIResponse} from "../types";
 
 function storeResponse(req: express.Request, res: APIResponse, next: express.NextFunction) {
-    const originalSend = res.send;
+    const originalJson = res.json;
 
-    res.send = function (body) {
+    res.json = function (body) {
         res.locals.responseBody = body;
 
-        return originalSend.call(this, body);
+        return originalJson.call(this, body);
     };
 
     next();
